0aa9911131a689969a548ad3ea4de3645e938887
TimelikeClosure <TimelikeClosure@users.noreply.github.com>
2018-09-14 23:48:56 -0700

    py - 00 - blink

---
+++ py/01/blink.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 11
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.output(ledPin, GPIO.LOW)
+    print ('using pin%d' %ledPin)
+
+def loop():
+    while True:
+        GPIO.output(ledPin, GPIO.HIGH)
+        print('led on')
+        time.sleep(1)
+        GPIO.output(ledPin, GPIO.LOW)
+        print('led off')
+        time.sleep(1)
+
+def destroy():
+    GPIO.output(ledPin, GPIO.LOW)
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
