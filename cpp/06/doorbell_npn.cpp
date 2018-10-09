ce30202956399e3716a97860a6928aa06bcec9d8
TimelikeClosure <TimelikeClosure@users.noreply.github.com>
2018-10-08 22:58:45 -0700

    cpp - 06 - doorbell

---
+++ cpp/06/doorbell_npn.cpp
@@ -0,0 +1,27 @@
+#include <wiringPi.h>
+#include <iostream>
+
+#define buzzerPin 0
+#define buttonPin 1
+
+int main(void){
+	if (wiringPiSetup() == -1){
+		std::cout << "setup wiringPi failed." << std::endl;
+		return 1;
+	}
+
+	pinMode(buzzerPin, OUTPUT);
+	pinMode(buttonPin, INPUT);
+
+	pullUpDnControl(buttonPin, PUD_UP);
+	while(1){
+		if (digitalRead(buttonPin) == LOW){
+			digitalWrite(buzzerPin, HIGH);
+		} else {
+			digitalWrite(buzzerPin, LOW);
+		}
+	}
+
+	return 0;
+}
+
