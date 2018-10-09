ea58c1012986099b976b18c9f1df1923a90dacfd
TimelikeClosure <TimelikeClosure@users.noreply.github.com>
2018-10-08 23:10:18 -0700

    py - 06 - doorbell

---
+++ py/06/doorbell.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+
+buzzerPin = 11
+buttonPin = 12
+
+def setup():
+    print("Program starting")
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(buzzerPin, GPIO.OUT)
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def loop():
+    while True:
+        if GPIO.input(buttonPin)==GPIO.LOW:
+            GPIO.output(buzzerPin, GPIO.HIGH)
+        else :
+            GPIO.output(buzzerPin, GPIO.LOW)
+
+def destroy():
+    GPIO.output(buzzerPin, GPIO.LOW)
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
